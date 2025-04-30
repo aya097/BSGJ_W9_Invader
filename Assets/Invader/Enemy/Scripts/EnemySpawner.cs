1965b039a01b45cdcc192551e887beaf76bac6ad
aya097 <akase097@gmail.com>
2025-04-30 13:57:40 +0900

    add EnemySpawnArranged

--- Assets/Invader/Enemy/Scripts/EnemySpawner.cs
+++ Assets/Invader/Enemy/Scripts/EnemySpawner.cs
@@ -10,20 +10,36 @@ namespace Invader.Enemy
     /// </summary>
     public class EnemySpawner
     {
+        const int _rowNum = 4;
+        const int _columnNum = 8;
+        readonly Vector2 _distance = new Vector2(1.5f, 1.5f);
+        readonly Vector2 _leftTop = new Vector2(-6f, 4f);
+
         readonly IEnemyCluster _enemyCluster;   // 作成したEnemyMonoを集約する
-        readonly GameObject _enemyMonoPrefab;   // EnemyMonoのPrefabを受け取る
+
 
         public EnemySpawner(GameObject enemyMonoPrefab, EnemyCluster enemyCluster)
         {
-            _enemyMonoPrefab = enemyMonoPrefab;
             _enemyCluster = enemyCluster;
-
-            SpawnEnemy(new Vector3(0, 0));  // 仮生成
+            SpawnEnemyArranged(enemyMonoPrefab, _leftTop, _rowNum, _columnNum, _distance);
         }
 
-        void SpawnEnemy(Vector3 position)
+        void SpawnEnemyArranged(GameObject enemyMonoPrefab, Vector2 leftTopPosition, int rowNum, int columnNum, Vector2 distance)
         {
-            var enemyMono = UnityEngine.GameObject.Instantiate(_enemyMonoPrefab, position, quaternion.identity);    // 引数の座標にEnemyを生成
+            for (int row = 0; row < rowNum; row++)  // 行繰り返し
+            {
+                for (int col = 0; col < columnNum; col++)   // 列繰り返し
+                {
+                    Vector2 position = leftTopPosition + distance * new Vector2(col, -row);  // 行列分離れた座標
+                    SpawnEnemy(enemyMonoPrefab, position);
+                }
+            }
+        }
+        void SpawnEnemy(GameObject enemyMonoPrefab, Vector2 position)
+        {
+            var enemyObject = GameObject.Instantiate(enemyMonoPrefab, position, quaternion.identity);    // 引数の座標にEnemyを生成
+            IEnemyMono enemyMono = enemyObject.GetComponent<EnemyMono>();   // EnemyMonoをインタフェースで取得する
+            _enemyCluster.AddEnemy(enemyMono);
         }
     }
 }
