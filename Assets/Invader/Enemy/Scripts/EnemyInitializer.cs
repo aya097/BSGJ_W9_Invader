af19cb31ad755a370582e595ac4ccfccd08acc5c
aya097 <akase097@gmail.com>
2025-05-06 13:40:31 +0900

    enemy move

--- Assets/Invader/Enemy/Scripts/EnemyInitializer.cs
+++ Assets/Invader/Enemy/Scripts/EnemyInitializer.cs
@@ -2,6 +2,9 @@
 
 using VContainer;
 using VContainer.Unity;
+using R3;
+using System.Threading;
+using System;
 
 namespace Invader.Enemy
 {
@@ -19,7 +22,15 @@ namespace Invader.Enemy
 
         void IStartable.Start()
         {
+            // Enemy生成
             _enemySpawner.Spawn();
+
+            float deltaTime = 0.2f;
+            // Enemy移動
+            Observable.Interval(TimeSpan.FromSeconds(deltaTime)).Subscribe(_ =>
+            {
+                _enemyMover.Move(deltaTime);
+            });
         }
     }
 }
